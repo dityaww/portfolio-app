91f9311c4292ba519a3ba70b9be80348fbc962fa
dityaww <111202012838@mhs.dinus.ac.id>
2024-09-12 02:06:12 +0700

    add listing arr mywork

---
+++ src/utils/ListWorks.js
@@ -0,0 +1,62 @@
+export const ListWorks = [
+    {
+        id: 1,
+        name: 'PT. Evolusi Bisnis Digital',
+        date: 'Jan 2024',
+        position: 'Web Programmer',
+        description: [
+            {
+                id: 1,
+                desc: 'lorem'
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: 'PT. Ruang Raya Indonesia',
+        date: 'Jan 2024',
+        position: 'Front-End Engineering',
+        description: [
+            {
+                id: 1,
+                desc: 'lorem'
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: 'PT. Impactbyte Teknologi Edukasi (Skilvul)',
+        date: 'Jan 2024',
+        position: 'Front-End Web Development',
+        description: [
+            {
+                id: 1,
+                desc: 'lorem'
+            }
+        ]
+    },
+    {
+        id: 4,
+        name: 'Digital Talent Scholarship KOMINFO X Cisco Networking Academy',
+        date: 'Jan 2024',
+        position: 'Fresh Graduate Academy Big Data using Python',
+        description: [
+            {
+                id: 1,
+                desc: 'lorem'
+            }
+        ]
+    },
+    {
+        id: 5,
+        name: 'PT. Sadasa Academy Indonesia',
+        date: 'Jan 2024',
+        position: 'Front-End Web Development',
+        description: [
+            {
+                id: 1,
+                desc: 'lorem'
+            }
+        ]
+    },
+]

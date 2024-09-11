952d3046a805326c74ee5351be2a8bf3644ad301
dityaww <111202012838@mhs.dinus.ac.id>
2024-09-12 02:04:55 +0700

    modified content ft experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -56,7 +56,7 @@ return (
                 <li className="mb-10 ms-4">
                     <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white"></div>
                     <time className="mb-1 text-sm font-normal leading-none text-gray-400">Apr 2024</time>
-                    <h2 className="text-lg font-semibold text-gray-900">PT Evolusi Bisnis Digital</h2>
+                    <h2 className="text-lg font-semibold text-gray-900">PT. Evolusi Bisnis Digital</h2>
                     <p className='text-gray-400 mb-3 italic'>Web Programmer</p>
                     <ul className="list-image-checkmark ms-4">
                         <li className="text-gray-600">

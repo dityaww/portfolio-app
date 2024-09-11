b70f233e59af21eec9c2825f9b76bfa7cb5bd042
dityaww <111202012838@mhs.dinus.ac.id>
2024-09-12 02:04:31 +0700

    modified in file about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,33 +6,20 @@ function About() {
     const { isDarkMode, setIsDarkMode } = useContext(DarkMode)
 
   return (
-    <div className={`${isDarkMode ? 'bg-neutral-600' : 'bg-neutral-100'}`}>
-        <div className="container mx-auto">
+    <div className={`${isDarkMode ? 'bg-neutral-600' : 'bg-gradient-to-r from-cyan-500 to-blue-600'} mb-10`}>
+        <div className="container mx-auto py-10">
             <div className="flex flex-col mb-5" data-aos="fade-right" data-aos-duration="1000">
-                <h1 className={`text-2xl font-extrabold font-caveat ${isDarkMode ? 'text-white' : 'text-neutral-800'}`}>About Me</h1>
-                <hr className='border-2 border-blue-600 w-10 rounded-full lg:ml-4'/>
+                <h1 className={`text-2xl font-extrabold font-caveat ${isDarkMode ? 'text-white' : 'text-white'}`}>About Me</h1>
+                <hr className='border-2 border-yellow-400 w-10 rounded-full lg:ml-4'/>
             </div>
             <div className="flex flex-col gap-2">
-                <p className={`${isDarkMode ? 'text-neutral-300' : 'text-neutral-800'}`} data-aos="fade-left" data-aos-duration="1000" data-aos-delay="100">
+                <p className={`${isDarkMode ? 'text-white' : 'text-white'}`} data-aos="fade-left" data-aos-duration="1000" data-aos-delay="100">
                     Hello, I'm Aditya Widyatmoko, a passionate web developer with a keen interest in frontend development. 
                     I pursue this career with a dedication to create amazing and captivating user experiences through responsive
                     and innovative design. In terms of experience and skills, I have a solid background in developing reliable 
                     and efficient web applications. My primary expertise lies in frontend development, mastering languages such 
                     as HTML, CSS, and JavaScript. I thrive on the challenges of crafting intuitive and appealing user interfaces.
-                </p>
-                <p className={`${isDarkMode ? 'text-neutral-300' : 'text-neutral-800'}`} data-aos="fade-left" data-aos-duration="1000" data-aos-delay="200">
-                    My focus extends to responsive design and a mobile-first approach. Recognizing the significance of a good user 
-                    experience across various devices, I consistently strive to ensure websites look and function seamlessly, whether 
-                    on desktop or mobile devices. As a developer, I believe problem-solving is key to success. I enjoy tackling technical 
-                    challenges and seek innovative solutions for every problem I encounter. My creativity permeates into every project 
-                    I undertake, creating unique visual aesthetics and experiences.
-                </p>
-                <p className={`${isDarkMode ? 'text-neutral-300' : 'text-neutral-800'}`} data-aos="fade-left" data-aos-duration="1000" data-aos-delay="300">
-                    I am committed to attention to detail and a dedication to quality. I always aim to produce clean, well-documented code 
-                    that adheres to industry best practices. Project success is my priority, and I am ready to work hard to achieve the best 
-                    results. If you're interested in collaboration or have a project requiring frontend skills, I'm excited to discuss further. 
-                    Thank you for visiting my profile!
-                </p>            
+                </p>         
             </div>
             <br />
             {/* <div className="flex flex-col gap-4">
